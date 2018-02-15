33f136a2dd87085083b56ceb978b85b6a8fe4a1d
Simon Frost <sdwfrost@gmail.com>
2018-02-15 07:43:59 +0000

    Modified

--- src/semiparDemoProcessModel0.cpp
+++ src/semiparDemoProcessModel0.cpp
@@ -8,33 +8,33 @@
 static const double MINY = 1e-12;
 
 using namespace arma;
-using namespace Rcpp; 
-using namespace std; 
+using namespace Rcpp;
+using namespace std;
 using namespace boost::numeric::odeint;
 
-typedef std::vector<double> state_type; 
+typedef std::vector<double> xstate_type;
 
 // globals
-vec g_betas; 
+vec g_betas;
 double g_gamma;
 
 ////////////////////////////////////////////////////////////////////////
 // solve change in psi vec (transition probabilities), not counting accrual
 // this is same as Q for the p_uk, but includes extra decay with Ne
-class ODE_sp0{ 
-private: 
+class ODE_sp0{
+private:
 	double t1;
 	double t0;
-	double dt; 
+	double dt;
 	int res;
 public:
 	ODE_sp0(double t0_, double t1_, int res_) : t0(t0_),t1(t1_),res(res_) {
-		dt = (t1 - t0) / (double)(res-1); 
+		dt = (t1 - t0) / (double)(res-1);
 	};
-	void operator() ( const state_type &x , state_type &dxdt , double t)//, const double /* t */ )
+	void operator() ( const xstate_type &x , xstate_type &dxdt , double t)//, const double /* t */ )
 	{
-		int i_t =  (int)std::max(0., std::min( (double)res * (t-t0) / (t1-t0) , (double)(res-1))); 
-		double beta = g_betas(i_t); 
+		int i_t =  (int)std::max(0., std::min( (double)res * (t-t0) / (t1-t0) , (double)(res-1)));
+		double beta = g_betas(i_t);
 		dxdt[0] = x[0] * ( beta - g_gamma);
 	}
 };
@@ -48,7 +48,7 @@ struct push_back_state_and_time
     push_back_state_and_time( std::vector< double > &states , std::vector< double > &times )
     : m_states( states ) , m_times( times ) { }
 
-    void operator()( const state_type &x , double t )
+    void operator()( const xstate_type &x , double t )
     {
         m_states.push_back( x[0] );
         m_times.push_back( t );
@@ -59,21 +59,21 @@ struct push_back_state_and_time
 //[[Rcpp::export()]]
 vec solve_semiPar0( double t0, double t1, int res, double y0, vec betas, double gamma, double eps_abs=1.0e-10, double eps_rel =1.0e-6)
 {
-	::g_betas = betas; 
+	::g_betas = betas;
 	::g_gamma = gamma;
-	
-	double dt = (t1-t0)/(res-1); 
-	
+
+	double dt = (t1-t0)/(res-1);
+
 	ODE_sp0 sp0(t0, t1, res);
 	std::vector<double> times; times.reserve(res);
 	std::vector<double> Ys; Ys.reserve(res);
 	push_back_state_and_time pbsat( Ys, times);
-	
+
 	//~ runge_kutta_fehlberg78
-	state_type x(1, y0);
-	size_t steps = integrate_const( make_controlled( eps_abs , eps_rel , runge_kutta_fehlberg78<state_type>() )
+	xstate_type x(1, y0);
+	size_t steps = integrate_const( make_controlled( eps_abs , eps_rel , runge_kutta_fehlberg78<xstate_type>() )
 	   , sp0 , x , t0 , t1 +dt/2., dt , pbsat );
-	
+
 	//~ List o;
 	//~ o["y"] = wrap( Ys ) ;
 	//~ o["times"] = wrap( times );
